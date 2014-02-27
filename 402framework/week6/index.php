8f01bb08394b5d67e23e12b31fb7c1770119a73b
ancientlives <ancientlives@gmail.com>
2014-02-27 16:15:34 -0600

    Update index.php

--- 402framework/week6/index.php
+++ 402framework/week6/index.php
@@ -13,7 +13,7 @@ require_once "frame/bootstrap.php";
 echo '<h3>'.$settings['project_title'].'</h3>';
 
 //test query the database and return single row result
-$field = array("4");
+$field = array("1");
 $db_results = DB::get_row("select * from users where userid=?", $field);
 print_r($db_results);
 
